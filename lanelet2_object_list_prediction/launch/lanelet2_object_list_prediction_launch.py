5e4673df940949a2bd598f80e26949b9d173314d
Christian Geller <88664444+cgeller@users.noreply.github.com>
2026-07-10 09:39:26 +0000

    rename input_object_list to tracked_object_list

--- lanelet2_object_list_prediction/launch/lanelet2_object_list_prediction_launch.py
+++ lanelet2_object_list_prediction/launch/lanelet2_object_list_prediction_launch.py
@@ -16,7 +16,7 @@ def generate_launch_description():
     """Generate launch description with resolved launch arguments."""
 
     remappable_topics = [
-        DeclareLaunchArgument("input_object_list_topic", default_value="~/input_object_list"),
+        DeclareLaunchArgument("tracked_object_list_topic", default_value="~/tracked_object_list"),
         DeclareLaunchArgument("object_list_topic", default_value="~/object_list"),
     ]
 
