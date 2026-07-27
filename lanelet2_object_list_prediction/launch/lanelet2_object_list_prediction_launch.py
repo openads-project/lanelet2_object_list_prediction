34016f79f78fafd27da91846714a5ac733ac0746
Raphael van Kempen <raphael.vankempen@ika.rwth-aachen.de>
2026-07-27 09:49:46 +0200

    import ifcondition

--- lanelet2_object_list_prediction/launch/lanelet2_object_list_prediction_launch.py
+++ lanelet2_object_list_prediction/launch/lanelet2_object_list_prediction_launch.py
@@ -8,6 +8,7 @@ import os
 from ament_index_python import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
+from launch.conditions import IfCondition
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node, SetParameter
 from tracetools_launch.action import Trace
